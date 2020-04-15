c9be2325f9c750a77b22f810985e12adb2a78c9a
sujitbidawe <bidawesujit@gmail.com>
2020-04-16 01:33:02 +0530

    added: getter and setter

--- src/main/java/censusanalyser/IndiaStateCodeCSV.java
+++ src/main/java/censusanalyser/IndiaStateCodeCSV.java
@@ -5,16 +5,48 @@ import com.opencsv.bean.CsvBindByName;
 public class IndiaStateCodeCSV {
 
     @CsvBindByName(column = "SrNo", required = true)
-    public String srNo;
+    private String srNo;
 
     @CsvBindByName(column = "State Name", required = true)
-    public String stateName;
+    private String stateName;
 
     @CsvBindByName(column = "TIN", required = true)
-    public String tin;
+    private String tin;
 
     @CsvBindByName(column = "StateCode", required = true)
-    public String stateCode;
+    private String stateCode;
+
+    public String getSrNo() {
+        return srNo;
+    }
+
+    public void setSrNo(String srNo) {
+        this.srNo = srNo;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public String getTin() {
+        return tin;
+    }
+
+    public void setTin(String tin) {
+        this.tin = tin;
+    }
+
+    public String getStateCode() {
+        return stateCode;
+    }
+
+    public void setStateCode(String stateCode) {
+        this.stateCode = stateCode;
+    }
 
     @Override
     public String toString() {
